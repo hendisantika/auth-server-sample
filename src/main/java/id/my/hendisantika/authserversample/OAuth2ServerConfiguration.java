8ae761eb589b218745153f035b3c471d04945e3d
hendisantika <hendisantika@yahoo.co.id>
2025-07-06 16:20:11 +0700

    feat: Add authorizationServerSecurityFilterChain🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/authserversample/OAuth2ServerConfiguration.java
+++ src/main/java/id/my/hendisantika/authserversample/OAuth2ServerConfiguration.java
@@ -1,6 +1,15 @@
 package id.my.hendisantika.authserversample;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,4 +24,26 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class OAuth2ServerConfiguration {
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer
+                .authorizationServer();
+
+        http.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher());
+        http.with(authorizationServerConfigurer, (authorizationServer) ->
+                authorizationServer
+                        .authorizationEndpoint(authorizationEndpoint ->
+                                authorizationEndpoint.authenticationProviders(configureAuthenticationValidator())
+                        )
+        );
+        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(withDefaults());
+        http.oauth2ResourceServer((resourceServer) -> resourceServer.jwt(withDefaults()));
+        http.exceptionHandling((exceptions) -> exceptions.defaultAuthenticationEntryPointFor(
+                new LoginUrlAuthenticationEntryPoint("/login"), createRequestMatcher()));
+
+
+        return http.build();
+    }
 }
