8b7fa5c8356e22a9f808d98d02dee0a4ce27fe46
hendisantika <hendisantika@yahoo.co.id>
2025-07-06 16:11:59 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/authserversample/AuthServerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.authserversample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
