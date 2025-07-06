d941b44e2236805571e02491ddc82241db5d17bb
hendisantika <hendisantika@yahoo.co.id>
2025-07-06 16:17:08 +0700

    feat: Add OAuth2ServerConfiguration🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/authserversample/OAuth2ServerConfiguration.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.authserversample;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : auth-server-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 06/07/25
+ * Time: 16.16
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class OAuth2ServerConfiguration {
+}
