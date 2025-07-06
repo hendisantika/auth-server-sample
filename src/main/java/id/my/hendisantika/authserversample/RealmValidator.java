0c3da0f5e8c778ead24d26196fb79461c7b824fe
hendisantika <hendisantika@yahoo.co.id>
2025-07-06 16:22:12 +0700

    feat: Add accept in RealmValidator🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/authserversample/RealmValidator.java
+++ src/main/java/id/my/hendisantika/authserversample/RealmValidator.java
@@ -1,6 +1,12 @@
 package id.my.hendisantika.authserversample;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationContext;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationException;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationToken;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
 import java.util.Set;
@@ -21,4 +27,33 @@ public class RealmValidator implements Consumer<OAuth2AuthorizationCodeRequestAu
 
     private final Map<String, Set<String>> allowedRealmsForClient = Map.of(
             "pkce-client", Set.of("invoice-realm", "order-realm", "product-realm"));
+
+    @Override
+    public void accept(OAuth2AuthorizationCodeRequestAuthenticationContext authenticationContext) {
+        OAuth2AuthorizationCodeRequestAuthenticationToken authorizationCodeRequestAuthentication =
+                authenticationContext.getAuthentication();
+        RegisteredClient registeredClient = authenticationContext.getRegisteredClient();
+        String requestedRealm = (String) authorizationCodeRequestAuthentication
+                .getAdditionalParameters()
+                .getOrDefault("realm", null);
+
+        if (requestedRealm == null || !allowedRealmsForClient.get(registeredClient.getClientId()).contains(requestedRealm)) {
+
+            OAuth2Error error = new OAuth2Error("invalid_realm", "OAuth 2.0 My Company Parameter: realm", "https://my-company.com/how-to-use-realm");
+
+            String redirectUri = StringUtils.hasText(authorizationCodeRequestAuthentication.getRedirectUri())
+                    ? authorizationCodeRequestAuthentication.getRedirectUri()
+                    : registeredClient.getRedirectUris().iterator().next();
+
+            OAuth2AuthorizationCodeRequestAuthenticationToken authorizationCodeRequestAuthenticationResult = new OAuth2AuthorizationCodeRequestAuthenticationToken(
+                    authorizationCodeRequestAuthentication.getAuthorizationUri(),
+                    authorizationCodeRequestAuthentication.getClientId(),
+                    (Authentication) authorizationCodeRequestAuthentication.getPrincipal(), redirectUri,
+                    authorizationCodeRequestAuthentication.getState(), authorizationCodeRequestAuthentication.getScopes(),
+                    authorizationCodeRequestAuthentication.getAdditionalParameters());
+            authorizationCodeRequestAuthenticationResult.setAuthenticated(true);
+
+            throw new OAuth2AuthorizationCodeRequestAuthenticationException(error, authorizationCodeRequestAuthenticationResult);
+        }
+    }
 }
